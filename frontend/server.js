9e61c3383bc59b616151d587400ad7a8a5be3a54
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-04-10 08:40:45 -0600

    server.js file for deploying

---
+++ frontend/server.js
@@ -0,0 +1,15 @@
+// Import express and path modules.
+const express = require('express');
+const path = require('path');
+// Create an express app.
+const app = express();
+// Serve the static files from the React app.
+app.use(express.static(path.join(__dirname, 'build')));
+// Redirect every request to the index.html 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+// Listen to the default port 80
+app.listen(80, function () {
+  console.log('Abegarage Car Mentainance app listening on port 80!');
+});
--- frontend/src/Contexts/AuthContext.js
+++ frontend/src/Contexts/AuthContext.js
@@ -1,90 +1,48 @@
-// // Import React and the Hooks we need here 
-// import React, { useState, useEffect, useContext } from "react";
-// // Import the Util function we created to handle the reading from the local storage 
-// import getAuth from '../util/auth';
-// // Create a context object  
-// const AuthContext = React.createContext();
-// // Create a custom hook to use the context
-// export const useAuth = () => {
-//   return useContext(AuthContext);
-// }
-// // Create a provider component  
-// export const AuthProvider = ({ children }) => {
-//   const [isLogged, setIsLogged] = useState(false);
-//   const [isAdmin, setIsAdmin] = useState(false);
-//   const [employee, setEmployee] = useState(null);
-//   const [customer, setCustomer] = useState(null);
-
-//   const value = { isLogged, isAdmin, setIsAdmin, setIsLogged, employee };
-
-//   useEffect(() => {
-//     // Retrieve the logged in user from local storage
-//     const loggedInEmployee = getAuth();
-//     // console.log(loggedInEmployee);
-//     // const loggedInCustomer = getAuth();
-//     // console.log(loggedInCustomer);
-
-//     loggedInEmployee.then((response) => {
-//       // console.log(response);
-//       if (response.employee_token) {
-//         setIsLogged(true);
-//         // 3 is the employee_role for admin
-//         if (response.employee_role === 3) {
-//           setIsAdmin(true);
-//         }
-//         setEmployee(response);
-//       }
-//     });
-//   }, []);
-//   return (
-//     <AuthContext.Provider value={value}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-
-
-
+// Import React and the Hooks we need here 
 import React, { useState, useEffect, useContext } from "react";
+// Import the Util function we created to handle the reading from the local storage 
 import getAuth from '../util/auth';
-
-// Create a context object
+// Create a context object  
 const AuthContext = React.createContext();
-
-// Custom hook to use the context
+// Create a custom hook to use the context
 export const useAuth = () => {
   return useContext(AuthContext);
 }
-
-// Provider component
+// Create a provider component  
 export const AuthProvider = ({ children }) => {
   const [isLogged, setIsLogged] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [user, setUser] = useState(null);
+  const [employee, setEmployee] = useState(null);
+  const [customer, setCustomer] = useState(null);
+
+  const value = { isLogged, isAdmin, setIsAdmin, setIsLogged, employee };
 
   useEffect(() => {
     // Retrieve the logged in user from local storage
-    const loggedInUser = getAuth();
-    
-    // Check if the user is logged in
-    if (loggedInUser && loggedInUser.token) {
-      setIsLogged(true);
-      // Check if the user is an admin
-      if (loggedInUser.role === "admin") {
-        setIsAdmin(true);
+    const loggedInEmployee = getAuth();
+    // console.log(loggedInEmployee);
+    // const loggedInCustomer = getAuth();
+    // console.log(loggedInCustomer);
+
+    loggedInEmployee.then((response) => {
+      // console.log(response);
+      if (response.employee_token) {
+        setIsLogged(true);
+        // 3 is the employee_role for admin
+        if (response.employee_role === 3) {
+          setIsAdmin(true);
+        }
+        setEmployee(response);
       }
-      // Set the user state
-      setUser(loggedInUser);
-    }
+    });
   }, []);
-
-  // Value to provide to the context
-  const value = { isLogged, isAdmin, user };
-
   return (
     <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+
+
+
