242ec8b25c853ddf483da6d948b2725f99d2db47
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-04-30 20:43:40 -0600

    NewOrder

---
+++ frontend/src/markup/pages/admin/NewOrder.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+// import the auth hook context
+import { useAuth } from "../../../Context/AuthContext";
+
+// import the login component
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+// import the admin menu component
+import AdminMenu from "../../components/Admin/AdminMenu/AdminMenu";
+
+import NewOrder1 from "../../components/Admin/NewOrder.jsx/NewOrder1";
+import { Link } from "react-router-dom";
+
+function NewOrder() {
+  const { isLogged, isAdmin } = useAuth();
+
+  if (isLogged) {
+    if (isAdmin) {
+      return (
+        <div>
+          <div className="container-fluid admin-pages">
+            <div className="row">
+              <div className="col-md-3 admin-left-side">
+                <AdminMenu />
+              </div>
+              <div className="col-md-9 admin-right-side">
+                <NewOrder1 />
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div class="not-found-container">
+          <div class="not-found-content">
+            <h2>
+              {" "}
+              You don't have the Permission to access the page you request!
+            </h2>
+            <Link class="back-home-link" to="/">
+              <span> Back to Home</span>
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <div>
+        <LoginForm />
+      </div>
+    );
+  }
+}
+
+export default NewOrder;
