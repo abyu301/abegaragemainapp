ef74bbd3c2f1b13803237896ed52c91b973b0bc8
abyu301 <152681521+gibabelewu@users.noreply.github.com>
2024-04-30 19:28:04 -0600

    Orders page

--- frontend/src/markup/pages/admin/Orders.js
+++ frontend/src/markup/pages/admin/Orders.js
@@ -1,24 +1,59 @@
-import React from 'react';
-// Import the AddEmployeeForm component 
-import CreateAnewOrder from '../../components/Admin/Orders/CreateAnewOrder';
-// Import the AdminMenu component 
-import AdminMenu from '../../components/Admin/AdminMenu/AdminMenu';
+import React from "react";
 
-function AddEmployee(props) {
-  return (
-    <div>
-      <div className="container-fluid admin-pages">
-        <div className="row">
-          <div className="col-md-3 admin-left-side">
-            <AdminMenu />
-          </div>
-          <div className="col-md-9 admin-right-side">
-            <CreateAnewOrder />
+// import the auth hook context
+import { useAuth } from "../../../Contexts/AuthContext";
+
+// import the login component
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+// import the admin menu component
+import AdminMenu from "../../components/Admin/AdminMenu/AdminMenu";
+
+import OrdersList from "../../components/Admin/OrdersList/OrdersList";
+import { Link } from "react-router-dom";
+
+function Orders() {
+  const { isLogged, isAdmin_manager, isAdmin, isAdmin_manager_employee } =
+    useAuth();
+
+  if (isLogged) {
+    if (isAdmin_manager || isAdmin || isAdmin_manager_employee) {
+      return (
+        <div>
+          <div className="container-fluid admin-pages">
+            <div className="row">
+              <div className="col-md-3 admin-left-side">
+                <AdminMenu />
+              </div>
+              <div className="col-md-9 admin-right-side">
+                <OrdersList />
+              </div>
+            </div>
           </div>
         </div>
+      );
+    } else {
+      return (
+        <div class="not-found-container">
+          <div class="not-found-content">
+            <h2>
+              {" "}
+              You don't have the Permission to access the page you request!
+            </h2>
+            <Link class="back-home-link" to="/">
+              <span> Back to Home</span>
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return (
+      <div>
+        <LoginForm />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default AddEmployee;
+export default Orders;
